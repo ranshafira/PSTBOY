43441d8fab35293168d9d2d57f1c02188a54a1af
ranshafira <raniashafirra@gmail.com>
2025-10-07 19:01:33 +0700

    benerin npm

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,24 @@
 import "./bootstrap";
-
 import Alpine from "alpinejs";
 
-import FullCalendar from "@fullcalendar/core";
+import { Calendar } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
 window.Alpine = Alpine;
-
 Alpine.start();
 
-document.addEventListener("DOMContentLoaded", function () {
-    var calendarEl = document.getElementById("calendar");
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: [dayGridPlugin, interactionPlugin],
-        initialView: "dayGridMonth",
-        events: "/admin/jadwal/events", // Memuat data jadwal dari API
-    });
-    calendar.render();
+document.addEventListener("DOMContentLoaded", () => {
+    const calendarEl = document.getElementById("calendar");
+
+    // Pastikan elemen #calendar ada di halaman
+    if (calendarEl) {
+        const calendar = new Calendar(calendarEl, {
+            plugins: [dayGridPlugin, interactionPlugin],
+            initialView: "dayGridMonth",
+            events: "/admin/jadwal/events", // endpoint untuk ambil data event
+        });
+
+        calendar.render();
+    }
 });
